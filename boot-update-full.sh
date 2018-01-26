ebf0361f75e5d7400f545c0c5a32701fbc20626d
Bradly Alan Teaford Cowan <bradly.cowan@gmail.com>
2018-01-25 22:02:27 -0500

    condensing

--- boot-update-full.sh
+++ boot-update-full.sh
@@ -6,7 +6,7 @@ DISTRO_NAME="budgie funtoo-test gentoo ubuntu"
 ROOT_MOUNT_DIR="/mnt/"
 
 budgie() {
-	SUBVOL_ROOT="@"
+	ROOT_SUBVOL="@"
 	ROOT_PART="/dev/sda5"
 	BOOT_PART="/dev/sda7"
 }
@@ -22,17 +22,17 @@ gentoo() {
 }
 
 ubuntu() {
-	SUBVOL_ROOT="@"
+	ROOT_SUBVOL="@"
 	ROOT_PART="/dev/sda12"
 	BOOT_PART="/dev/sda11"
 }
 
 check_mount_options() {
-	if [[ -n "${SUBVOL_ROOT}" ]]; then
-		SUBVOL_ROOT="-o subvol=${SUBVOL_ROOT} "
+	if [[ -n "${ROOT_SUBVOL}" ]]; then
+		ROOT_SUBVOL="-o subvol=${ROOT_SUBVOL} "
 	fi
-	if [[ -n "${SUBVOL_BOOT}" ]]; then
-		SUBVOL_BOOT="-o subvol=${SUBVOL_BOOT} "
+	if [[ -n "${BOOT_SUBVOL}" ]]; then
+		BOOT_SUBVOL="-o subvol=${BOOT_SUBVOL} "
 	fi
 	if [[ -n "${ROOT_LABEL}" ]]; then
 		ROOT_LABEL="LABEL=${ROOT_LABEL}"
@@ -43,7 +43,7 @@ check_mount_options() {
 }
 
 unset_mount_vars() {
-	for i in "ROOT_PART BOOT_PART ROOT_LABEL BOOT_LABEL SUBVOL_ROOT SUBVOL_BOOT"; do
+	for i in {ROOT,BOOT}{_PART,_LABEL,_SUBVOL}; do
 		unset $i
 	done
 }
@@ -53,11 +53,11 @@ chroot_mount() {
 		echo -e "${DISTRO} ROOT and BOOT already mounted...skipping"
 	else
 		[[ $(findmnt -M "${ROOT_MOUNT_DIR}${DISTRO}") ]] || \
-			{ mount ${SUBVOL_ROOT}${ROOT_LABEL}${ROOT_PART} ${ROOT_MOUNT_DIR}${DISTRO}; \
+			{ mount ${ROOT_SUBVOL}${ROOT_LABEL}${ROOT_PART} ${ROOT_MOUNT_DIR}${DISTRO}; \
 			echo -e "Mounting ${DISTRO} ROOT"; \
 			NEW_ROOT_MOUNTS="${NEW_ROOT_MOUNTS} ${ROOT_MOUNT_DIR}${DISTRO}"; }
 		[[ $(findmnt -M "${ROOT_MOUNT_DIR}${DISTRO}/boot") ]] || \
-			{ mount ${SUBVOL_BOOT}${BOOT_LABEL}${BOOT_PART} ${ROOT_MOUNT_DIR}${DISTRO}/boot; \
+			{ mount ${BOOT_SUBVOL}${BOOT_LABEL}${BOOT_PART} ${ROOT_MOUNT_DIR}${DISTRO}/boot; \
 			echo -e "Mounting ${DISTRO} BOOT \n"; \
 			NEW_BOOT_MOUNTS="${NEW_BOOT_MOUNTS} ${ROOT_MOUNT_DIR}${DISTRO}/boot"; }
 	fi
